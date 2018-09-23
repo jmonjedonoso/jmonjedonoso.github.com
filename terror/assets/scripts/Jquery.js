4cbab1b57786f37136c42d5369db2cf9ea6a2b18
jmonjedonoso <42706864+jmonjedonoso@users.noreply.github.com>
2018-09-23 00:27:55 -0300

    ranking

---
+++ terror/assets/scripts/Jquery.js
@@ -0,0 +1,38 @@
+//alert("hola mundo js");
+//console.log("hola mundo js en consola");
+//DOM (document object model)
+
+//let element = document.queryselector(".fa-star");
+//los objetos dentro del DOM se conocen como Nodos
+
+// let elementList = document.querySelectorAll(".fa-star");
+
+// elementList.forEach(function(element){
+//     element.addEventListener("click",function(){
+//          alert("estrellita clickeada");
+//          element.classList.remove("far");
+//          element.classList.add("fas");
+//     })
+ 
+// });
+
+
+
+
+let closes = document.querySelectorAll(".close");
+let content = document.querySelector(".content");
+
+closes.forEach(function(close){
+    close.addEventListener('click',function(ev) {
+        
+   
+        ev.preventDefault();
+        content.classList.remove("animate");
+        content.classList.remove("fadeInRight");
+        content.classList.add("animate");
+        content.classList.add("fadeOutRight");
+        setTimeout(() => {
+           location.href="/";
+        },3000)
+    });  
+})
